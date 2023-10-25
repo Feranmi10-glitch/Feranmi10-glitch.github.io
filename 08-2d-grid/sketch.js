d28423472fab123ade6c575c4e515607122ac4a8
Feranmi10-glitch <146806749+Feranmi10-glitch@users.noreply.github.com>
2023-10-25 11:14:37 -0600

    Upgraded 2d grid array

--- 08-2d-grid/sketch.js
+++ 08-2d-grid/sketch.js
@@ -3,16 +3,16 @@
 // October 24, 2023.
 
 let grid;
-
 let cellSize;
 const GRID_SIZE = 15;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  if(height > width){  
-    cellSize = width /GRID_SIZE;
+
+  if (height > width) {
+    cellSize = width/GRID_SIZE;
   }
-  else{
+  else {
     cellSize = height/GRID_SIZE;
   }
   grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
@@ -23,39 +23,39 @@ function draw() {
   displayGrid();
 }
 
-function keyTyped(){
-  // if (key === "r"){
-  //   grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
-  // }
-  // else if(key === "e"){
-  //   grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
-  // }
+function keyTyped() {
+  if (key === "r") {
+    grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
+  }
+  else if (key === "e") {
+    grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
+  }
 }
 
-
-function displayGrid(){ 
-  for (let y = 0; y < GRID_SIZE; y++){
-    for (let x = 0; x < GRID_SIZE; x++){
-      if(grid[y][x] === 0){
+function displayGrid() {
+  for (let y = 0; y < GRID_SIZE; y++) {
+    for (let x = 0; x < GRID_SIZE; x++) {
+      if (grid[y][x] === 0) {
         fill("white");
-        if (grid[y][x] === 1){
-          fill("black");
-        }
-        rect(x * cellSize, y * cellSize, cellSize, cellSize);
       }
+      if (grid[y][x] === 1) {
+        fill("black");
+      }
+      rect(x * cellSize, y * cellSize, cellSize, cellSize);
     }
   }
 }
 
-function generateRandomGrid(cols, rows){
+
+function generateRandomGrid(cols, rows) {
   let randomArray = [];
-  for (let y = 0; y < cols; y++){
+  for (let y = 0; y < cols; y++) {
     randomArray.push([]);
-    for (let x = 0; y < rows; x++){
-      if(random(100) < 50){
+    for (let x = 0; x < rows; x++) {
+      if (random(100) < 50) {
         randomArray[y].push(0);
       }
-      else{
+      else {
         randomArray[y].push(1);
       }
     }
@@ -63,11 +63,11 @@ function generateRandomGrid(cols, rows){
   return randomArray;
 }
 
-function generateEmptyGrid(cols, rows){
+function generateEmptyGrid(cols, rows) {
   let randomArray = [];
-  for (let y = 0; y < cols; y++){
+  for (let y = 0; y < cols; y++) {
     randomArray.push([]);
-    for (let x = 0; y < rows; x++){
+    for (let x = 0; x < rows; x++) {
       randomArray[y].push(0);
     }
   }
