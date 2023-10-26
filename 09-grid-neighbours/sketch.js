e99bda12020a67928cd272bedfc99f1bc9c2c7e7
Feranmi10-glitch <146806749+Feranmi10-glitch@users.noreply.github.com>
2023-10-26 12:19:05 -0600

    Made the game of life

--- 09-grid-neighbours/sketch.js
+++ 09-grid-neighbours/sketch.js
@@ -1,4 +1,4 @@
-// Project Title
+// Game Of LIfe
 // Your Name
 // Date
 //
@@ -6,8 +6,9 @@
 // - describe what you did to take this project "above and beyond"
 
 let grid;
-const GRID_SIZE = 15;
+const GRID_SIZE = 30;
 let cellSize;
+let autoplay = true;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -20,23 +21,79 @@ function setup() {
   }
 
 
-  grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
+  grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
 }
 
 function draw() {
   background(220);
+  if (autoplay && frameCount % 10 === 0){
+    grid = nextTurn();
+  }
   displayGrid();
 }
+
+function keyTyped(){
+  if (key === "r"){
+    grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
+  }
+  else if (key === "e"){
+    grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
+  }
+  else if (key === " "){
+    grid = nextTurn();
+  }
+  else if(key === "a"){
+    autoplay =!autoplay;
+  }
+}
+
+function nextTurn(){
+  let nextTurnGrid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
+  // look at every cell
+  for (let y = 0; y < GRID_SIZE; y++){
+    for(let x = 0; y < GRID_SIZE; x++){
+      // count neighbours 
+      let neighbours = 0;
+
+      // look at all cells around in a 3x3  grid 
+      for(let i = -1; i <= 1; i++){
+        for(let j = -1; j <= 1; j++){
+          //detect edge cases
+          if(y+i >= 0 && y+i < GRID_SIZE && x+j >= 0 && x+j < GRID_SIZE){
+            neighbours += grid[y+1][x+j];
+          }
+        }
+      }
+
+      // be careful about counting self
+      neighbours -= grid[y][x];
+
+      //apply rules
+      if (grid[y][x] === 0) { //dead
+        if (neighbours === 3) {
+          //new birth
+          nextTurnGrid[y][x] = 1;
+        }
+        else {
+          //stay dead
+          nextTurnGrid[y][x] = 0;
+        }
+      }
+    }
+  }
+  return nextTurnGrid;
+}
+
 
 function mousePressed(){
   let y = Math.floor(mouseY/cellSize);
   let x = Math.floor(mouseX/cellSize);
 
   toggleCell(x, y);
-  toggleCell(x, y-1);
-  toggleCell(x, y+1);
-  toggleCell(x-1, y);
-  toggleCell(x+1, y);
+  // toggleCell(x, y-1);
+  // toggleCell(x, y+1);
+  // toggleCell(x-1, y);
+  // toggleCell(x+1, y);
 }
 
 function toggleCell(x, y){
