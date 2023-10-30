ffe0e56ca6a3f27fa4dcdba0e2d23ab00824887f
Feranmi10-glitch <146806749+Feranmi10-glitch@users.noreply.github.com>
2023-10-30 12:17:14 -0600

    Made grid for tic tac toe

--- Tic-Tac-Toe/sketch.js
+++ Tic-Tac-Toe/sketch.js
@@ -6,16 +6,65 @@
 // - describe what you did to take this project "above and beyond"
 
 let grid;
-let cellsize;
-const GRID_SIZE = 100;
+let cellSize;
+const GRID_SIZE = 3;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
-
+  if (height > width) {
+    cellSize = width/GRID_SIZE;
+  }
+  else {
+    cellSize = height/GRID_SIZE;
+  }
+  grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
 }
 
 function draw() {
   background(220);
+  displayGrid();
+}
+
+function mousePressed(){
+  let y = Math.floor(mouseY/cellSize);
+  let x = Math.floor(mouseX/cellSize);
+  if (grid[y][x] === 0){
+    grid[y][x] = 1;
+  }
+  else{
+    grid[y][x] = 0;
+  }
+  
+}
+
+function keyTyped() {
+  if (key === "e") {
+    grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
+  }
+}
+
+function displayGrid() {
+  for (let y = 0; y < GRID_SIZE; y++) {
+    for (let x = 0; x < GRID_SIZE; x++) {
+      if (grid[y][x] === 0) {
+        fill("white");
+      }
+      if (grid[y][x] === 1) {
+        fill("black");
+      }
+      rect(x * cellSize, y * cellSize, cellSize, cellSize);
+    }
+  }
+}
+
+function generateEmptyGrid(cols, rows) {
+  let randomArray = [];
+  for (let y = 0; y < cols; y++) {
+    randomArray.push([]);
+    for (let x = 0; x < rows; x++) {
+      randomArray[y].push(0);
+    }
+  }
+  return randomArray;
 }
